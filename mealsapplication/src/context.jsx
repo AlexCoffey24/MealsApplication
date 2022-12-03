da69bc115addf23e489a0b92d839490a30b41f9c
AlexCoffey24 <AlexCoffey24@gmail.com>
2022-12-02 23:21:37 -0500

    commit

--- mealsapplication/src/context.jsx
+++ mealsapplication/src/context.jsx
@@ -1,9 +1,34 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 
 const AppContext = React.createContext();
 
+const allMealsUrl = "www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata";
+const randomMealUrl = "www.themealdb.com/api/json/v1/1/random.php";
+
 const AppProvider = ({ children }) => {
-  return <AppContext.Provider value="hello">{children}</AppContext.Provider>;
+  const fetchData = async () => {
+    try {
+      const response = await fetch("https://randomuser.me/api/");
+      const data = response.json();
+      console.log(data);
+    } catch (error) {
+      console.log("error");
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <AppContext.Provider value={{ name: "john", role: "student" }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => {
+  return useContext(AppContext);
 };
 
 export { AppContext, AppProvider };
